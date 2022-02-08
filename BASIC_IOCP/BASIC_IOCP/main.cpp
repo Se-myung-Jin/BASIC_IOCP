d9c72336960a682948216b32328c7d1f253c71f4
SeMyungJin <tpaud94@naver.com>
2022-02-08 16:41:08 +0900

    main source code and project setting change

--- BASIC_IOCP/BASIC_IOCP/main.cpp
+++ BASIC_IOCP/BASIC_IOCP/main.cpp
@@ -1,4 +1,21 @@
+#include "IOCompletionPort.h"
+
+const UINT16 SERVER_PORT = 8088;
+const UINT16 MAX_CLIENT = 200;
+
 int main()
 {
+	IOCompletionPort iocp;
+
+	iocp.InitSocket();
+
+	iocp.ListenSocket(SERVER_PORT);
+
+	iocp.StartServer(MAX_CLIENT);
+
+	getchar();
+
+	iocp.EndServer();
+
 	return 0;
 }
